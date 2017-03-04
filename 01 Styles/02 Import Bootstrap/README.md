cc631f8124e2f74fcd2d23a6cbedaa0814bce3ea
nasdan <daniel.sanchez@lemoncode.net>
2017-03-04 21:57:18 +0100

    Copy files from sample 01 Custom CSS

---
+++ 01 Styles/02 Import Bootstrap/README.md
@@ -0,0 +1,31 @@
+# 02 Import Bootstrap
+
+In this demo we will install and configure webpack to import the well known
+bootstrap css library.
+
+In this demo will import Bootstrap, and use a css component.
+
+We will start from sample _01 Styles/01 Custom CSS_.
+
+Summary steps:
+ - Install bootstrap.
+ - Use a jumbotron element from bootstrap in our HTML.
+ - Import the css library.
+ - Check that we get errors when running webpack.
+ - Install additional loaders in order to manage fonts and other
+ files required by bootstrap.
+ - Check results.
+
+# Steps to build it
+
+## Prerequisites
+
+Prerequisites, you will need to have nodejs installed in your computer. If you want to follow this step guides you will need to take as starting point sample _01 Custom CSS_.
+
+## steps
+
+- `npm install` to install previous sample packages:
+
+```
+npm install
+```
---
+++ 01 Styles/02 Import Bootstrap/webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+  entry: {
+    app: './students.js',
+    appStyles: [
+      './mystyles.css',
+    ],
+    vendor: [
+      'jquery',
+    ],
+  },
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: '[chunkhash].[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+          },
+        }),
+      },
+    ],
+  },
+  // For development https://webpack.js.org/configuration/devtool/#for-development
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+			hash: true,
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new ExtractTextPlugin({
+      filename: '[chunkhash].[name].css',
+      disable: false,
+      allChunks: true,
+    }),
+  ],
+};
