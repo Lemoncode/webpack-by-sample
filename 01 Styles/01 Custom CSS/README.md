f4ab2884ac3f326adf611f1d6f955fb622263577
nasdan <daniel.sanchez@lemoncode.net>
2017-03-04 20:15:37 +0100

    Copy files from sample 00 Intro/04 JQuery

---
+++ 01 Styles/01 Custom CSS/README.md
@@ -0,0 +1,27 @@
+# 01 Custom CSS
+
+Let's get started working with styles.
+
+In this demo will create a custom CSS file (it will contain a simple css class
+that will setup a background color to red).
+
+We will start from sample _00 Intro/04 JQuery_.
+
+Summary steps:
+ - Create a custom css file.
+ - Install style loader and css loader packages.
+ - Configure webpackconfig.
+
+ # Steps to build it
+
+ ## Prerequisites
+
+ Prerequisites, you will need to have nodejs installed in your computer. If you want to follow this step guides you will need to take as starting point sample _04 JQuery_.
+
+ ## steps
+
+ - `npm install` to install previous sample packages:
+
+ ```
+ npm install
+ ```
---
+++ 01 Styles/01 Custom CSS/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var basePath = __dirname;
+
+module.exports = {
+  entry: {
+    app: './students.js',
+    vendor: [
+      'jquery',
+    ],
+  },
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: '[chunkhash].[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  // For development https://webpack.js.org/configuration/devtool/#for-development
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+			hash: true,
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+  ],
+};
