defb2996c061c2df6ba294ddd90342f2f82d067a
dailymp@gmail.com <dailymp@gmail.com>
2018-03-26 12:34:43 +0200

    All the things from the guide, completed.

---
+++ version 4/01 Styles/03 sass/src/students.ts
@@ -0,0 +1,10 @@
+// Let's use some ES6 features
+import {getAvg} from "./averageService";
+
+$('body').css('background-color', 'lightSkyBlue');
+
+const scores: number[] = [90, 75, 60, 99, 94, 30];
+const averageScore: number = getAvg(scores);
+const messageToDisplay: string = `average score ${averageScore}`;
+
+document.write(messageToDisplay);
