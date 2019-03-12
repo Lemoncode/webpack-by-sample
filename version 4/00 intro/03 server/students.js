490299633d0dbeb0f06a808ccb111c82aeb52f56
Braulio <braulio.diez@gmail.com>
2019-03-12 17:01:54 +0100

    03 server completed

--- version 4/00 intro/03 server/students.js
+++ version 4/00 intro/03 server/students.js
@@ -1,6 +1,7 @@
 import {getAvg} from "./averageService";
 
 const scores = [90, 75, 60, 99, 94, 30];
+
 const averageScore = getAvg(scores);
 
 const messageToDisplay = `average score ${averageScore}`;
