a3eba232ecc0d7c3314f90cf986703c59e104f55
Braulio <braulio.diez@gmail.com>
2019-03-12 17:29:02 +0100

    styles 01 custom css ported

--- version 4/01 Styles/01 custom-css/webpack.config.js
+++ version 4/01 Styles/01 custom-css/webpack.config.js
@@ -1,54 +1,49 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var webpack = require('webpack');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    app: './students.js',
-    appStyles: [
-      './mystyles.css',
-    ],
-    vendor: [
-      'jquery',
-    ],
+    app: "./students.js",
+    appStyles: ["./mystyles.css"],
+    vendor: ["jquery"]
   },
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: "[name].[chunkhash].js"
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendor: {
-          chunks: 'initial',
-          name: 'vendor',
-          test: 'vendor',
+          chunks: "initial",
+          name: "vendor",
+          test: "vendor",
           enforce: true
-        },
+        }
       }
     }
   },
+
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-      },
-    ],
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
   },
   plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html', //Name of file in ./dist/
-      template: 'index.html', //Name of template in ./src
-      hash: true,
+      filename: "index.html", //Name of file in ./dist/
+      template: "index.html", //Name of template in ./src
+      hash: true
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
@@ -57,6 +52,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    }),
-  ],
+    })
+  ]
 };
