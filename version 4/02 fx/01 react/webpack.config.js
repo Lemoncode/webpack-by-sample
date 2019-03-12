e95bd791dadb10d86bbc06947a83aecc57751989
Braulio <braulio.diez@gmail.com>
2019-03-12 22:17:01 +0100

    react sample updated

--- version 4/02 fx/01 react/webpack.config.js
+++ version 4/02 fx/01 react/webpack.config.js
@@ -1,75 +1,64 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var webpack = require('webpack');
-var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+var path = require("path");
 var basePath = __dirname;
 
-
 module.exports = {
-  context: path.join(basePath, 'src'),
+  context: path.join(basePath, "src"),
   resolve: {
-         extensions: ['.js', '.jsx'],
-  },  
+    extensions: [".js", ".jsx"]
+  },
   entry: {
-    app: './students.jsx',
-    vendor: [
-      'react',
-      'react-dom',
-    ],
-    vendorStyles: [
-      '../node_modules/bootstrap/dist/css/bootstrap.css',
-    ],
+    app: "./students.jsx",
+    vendor: ["react", "react-dom"],
+    vendorStyles: ["../node_modules/bootstrap/dist/css/bootstrap.css"]
   },
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: "[name].[chunkhash].js"
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendor: {
-          chunks: 'initial',
-          name: 'vendor',
-          test: 'vendor',
+          chunks: "initial",
+          name: "vendor",
+          test: "vendor",
           enforce: true
-        },
+        }
       }
     }
   },
+
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
+
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
   },
   plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html', //Name of file in ./dist/
-      template: 'index.html', //Name of template in ./src
-      hash: true,
+      filename: "index.html", //Name of file in ./dist/
+      template: "index.html", //Name of template in ./src
+      hash: true
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    }),
-  ],
+    })
+  ]
 };
